14ef08f3c5176b20e739930aeb2e62d243e22f5f
Nikolay Anatoliev Lilkin <nikolayanatolievlilkin@gmail.com>
2022-04-12 02:21:14 +0300

    7th test runs

--- server.js
+++ server.js
@@ -12,9 +12,15 @@ mongoose.connect(process.env.DATABASE,{
   useUnifiedTopology: true,
   useNewUrlParser: true
 });
-var Users = new mongoose.Schema({user:String});
+var Users = new mongoose.Schema({username:String});
 var usersModel = mongoose.model("Users", Users);
-
+var Exercises = new mongoose.Schema({
+  user:Users,
+  description: String,
+  duration: Number,
+  date: Date
+});
+var exerciesModel = mongoose.model("Exercises",Exercises);
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -25,16 +31,16 @@ app.get('/hello', (req,res) => {
 })
 
 app.post('/api/users',urlencodedParser,async function(req,res){
-  let dbUser = await usersModel.findOne({user:req.body.username});
+  let dbUser = await usersModel.findOne({username:req.body.username});
   if(dbUser === null){
     let newCreatedUser= await usersModel.create({
-      user:req.body.username
+      username:req.body.username
     });
     console.log("Created new user " + req.body.username);
-    res.json({user:req.body.username,_id:newCreatedUser._id});
+    res.send({username:req.body.username,_id:newCreatedUser._id});
   }
   else{
-    res.json({user:dbUser.user,_id:dbUser._id});
+    res.send({username:dbUser.username,_id:dbUser._id});
   }
 });
 
@@ -42,6 +48,54 @@ app.get('/api/users',async function (req, res){
   let users = await usersModel.find();
   res.send(users);
 });
+
+app.post('/api/users/:_id/exercises',urlencodedParser,async function (req,res){
+  let regEx = /^\d{4}-\d{2}-\d{2}$/;
+  let user = await usersModel.findById(req.params._id);
+  console.log(user);
+  if(user === null){
+    res.send({Error:"No such id"});
+  }
+  else{
+    console.log("The date is " + req.body.date);
+    if(req.body.date === ""){
+      let newExercise = await exerciesModel({
+      user: user,
+      description: req.body.description,
+      duration: req.body.duration,
+      date: new Date()
+    });
+    console.log(newExercise);
+    res.send({username: newExercise.user.username,
+             description: newExercise.description,
+             duration: newExercise.duration,
+             date: newExercise.date.toDateString() 
+             });
+    }
+    else if(req.body.date.match(regEx)){
+      let newExercise = await exerciesModel({
+      user: user,
+      description: req.body.description,
+      duration: req.body.duration,
+      date: new Date()
+    });
+    console.log(newExercise);
+    res.send({username: newExercise.user.username,
+             description: newExercise.description,
+             duration: newExercise.duration,
+             date: newExercise.date.toDateString() 
+             });
+    }
+    else{
+      res.send({Error:"Invalid Date"});
+    }
+    
+  }
+  
+  //console.log(user.username);
+  //res.send(user);
+});
+
 
 mongoose.connection.once('open',() => {
   console.log("Connected to MongoDB");
