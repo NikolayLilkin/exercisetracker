935b29e63e38af5e0e5796618c26aa8f0ba23eb7
Nikolay Anatoliev Lilkin <nikolayanatolievlilkin@gmail.com>
2022-04-12 22:58:14 +0300

    commit

--- server.js
+++ server.js
@@ -50,26 +50,10 @@ app.get('/api/users',async function (req, res){
 
 app.post('/api/users/:_id/exercises',urlencodedParser,async function (req,res){
   let dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-  let user;
-  try {
-    user = await usersModel.findById(req.params._id);
-  } catch (error) {
-    res.send({Error:"Invalid user"});
-    return
-  }
+  let user = await usersModel.findById(req.params._id);
   let description = req.body.description;
-  let duration = req.body.duration;
-  let date = req.body.date;
-  if(!(date.match(dateRegex)) && date !==""){
-    res.send({Error:"You entered unvalid date"})
-    return;
-  }
-  if(date === ""){
-    date = new Date();
-  }
-  else{
-    date = new Date(date);
-  }
+  let duration = parseInt(req.body.duration);
+  let date = req.body.date?new Date(req.body.date).toDateString() : new Date().toDateString();
   let exerciesArray=await exerciesModel.findOne({user:user});
   if(exerciesArray === null){
     console.log("Creating new Item in the exerciesModel");
@@ -79,7 +63,7 @@ app.post('/api/users/:_id/exercises',urlencodedParser,async function (req,res){
       log:[{
         description:description,
         duration:duration,
-        date:date.toDateString()
+        date:date
       }]
     });
     console.log("Newly created Exercise")
@@ -90,7 +74,7 @@ app.post('/api/users/:_id/exercises',urlencodedParser,async function (req,res){
     let count=exerciesArray.count;
     log.push({description:description,
               duration:duration,
-              date:date.toDateString()
+              date:date
             });       
     exerciesArray.log = log;
     exerciesArray.count = count + 1;
@@ -99,24 +83,39 @@ app.post('/api/users/:_id/exercises',urlencodedParser,async function (req,res){
     console.log(exerciesArray); 
   }
   res.send({
-    user: user.username,
-    description: description,
+    _id:user._id,
+    username:user.username,
+    date: date,
     duration: duration,
-    date: date.toDateString()
+    description: description
   });
 });
 
 app.get('/api/users/:_id/logs',urlencodedParser,async function(req,res){
-  let user = await usersModel.findById((req.params._id));
-  if(user === null){
-    res.send({Error:"There is no such user"});
-    return
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = req.query.limit;
+  let user = await usersModel.findById(req.params._id);
+  let exerciesArray=await   exerciesModel.findOne({user:user});
+  let log = exerciesArray.log;
+  if (from){
+    const fromDate = new Date(from)
+    log = log.filter(exe => new Date(exe.date)>= fromDate)
   }
-  else{
-    let exerciesArray=await exerciesModel.findOne({user:user});
-    res.send(exerciesArray);
-  } 
-})
+  if (to){
+    const toDate = new Date(to)
+    log = log.filter(exe => new Date(exe.date)<= toDate)
+  }
+  if(limit){
+    log = log.slice(0,limit)
+  }
+  res.send({
+        _id:user._id,
+        username: user.username,
+        count: exerciesArray.count,
+        log: log
+      });
+  });
 
 mongoose.connection.once('open',() => {
   console.log("Connected to MongoDB");
